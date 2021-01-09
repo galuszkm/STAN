89ef1dd5fe3b01a418ca434e0fe05fedbf380e37
Michal <michal.galuszka1@gmail.com>
2021-01-09 19:19:03 +0100

    Create Analysis.cs

---
+++ src/STAN_Database/Analysis.cs
@@ -0,0 +1,87 @@
+﻿using ProtoBuf;
+
+namespace STAN_Database
+{
+    [ProtoContract(SkipConstructor = true)]
+    public class Analysis
+    {
+        [ProtoMember(1)] private string Type;
+        [ProtoMember(2)] private string LinSolver;
+        [ProtoMember(3)] private double LinSolverTolerance;
+        [ProtoMember(4)] private int LinSolverIterMax;
+        [ProtoMember(5)] private int IncNumb;
+        [ProtoMember(6)] private int Result_StepNo;
+
+        public Analysis()
+        {
+            Type = "Linear_Statics";
+            LinSolver = "CG";
+            LinSolverTolerance = 1.0e-6;
+            LinSolverIterMax = 0;
+            IncNumb = 0;
+
+            // Result status
+            Result_StepNo = 0;
+        }
+        
+        public void SetAnalysisType(string type)
+        {
+            Type = type;
+        }
+
+        public string GetAnalysisType()
+        {
+            return Type;
+        }
+
+        public void SetLinSolver(string linsol)
+        {
+            LinSolver = linsol;
+        }
+
+        public string GetLinSolver()
+        {
+            return LinSolver;
+        }
+
+        public void SetLinSolverTolerance(double tolerance)
+        {
+            LinSolverTolerance = tolerance;
+        }
+
+        public double GetLinSolverTolerance()
+        {
+            return LinSolverTolerance;
+        }
+
+        public void SetLinSolverMaxIter(int maxiter)
+        {
+            LinSolverIterMax = maxiter;
+        }
+
+        public int GetLinSolverMaxIter()
+        {
+            return LinSolverIterMax;
+        }
+
+        public void SetIncNumb(int inc)
+        {
+            IncNumb = inc;
+        }
+
+        public int GetIncNumb()
+        {
+            return IncNumb;
+        }
+
+        public int GetResultStepNo()
+        {
+            return Result_StepNo;
+        }
+
+        public void SetResultStepNo(int inc)
+        {
+            Result_StepNo = inc;
+        }
+    }
+}
